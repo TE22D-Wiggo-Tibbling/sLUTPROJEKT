80b53ca48d4de0fb1d9f6eb32af1b9d1a23ca2ca
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2024-03-19 14:17:08 +0100

    hghjgjjhhggjjgjgjg

--- sLUTPROJEKT/Program.cs
+++ sLUTPROJEKT/Program.cs
@@ -6,20 +6,26 @@ using Raylib_cs;
         int screenWidth = 500;
         int screenHeight = 900;
 
+        Random random = new Random();
+
         Raylib.InitWindow(screenWidth, screenHeight, "Hello World");
         Raylib.SetTargetFPS(60);
 
         Player player = new Player();
 
+        Plattform plattform = new Plattform();
 
-        Rectangle platform = new Rectangle(screenWidth/2,800,80,5);
+        for (int i = 0; i < screenHeight; i++)
+        {
+
+        }
 
 
         while (!Raylib.WindowShouldClose())
         {
-
+        plattform.skap();   
             player.Rörelse();
-           if( Raylib.CheckCollisionRecs(player.characterRec,platform)&&player.movement.Y>0){
+           if( Raylib.CheckCollisionRecs(player.characterRec,plattform.size)&&player.movement.Y>0){
             player.movement.Y = player.bounce;
            }
 
@@ -27,14 +33,29 @@ using Raylib_cs;
             Raylib.ClearBackground(Color.Blue);
 
             player.Rita();
+            plattform.rita();
 
            
-            Raylib.DrawRectangleRec(platform, Color.Green);
             Raylib.EndDrawing();
         }
 
 
 public class Plattform
 {
+    static Random random1 = new Random();
+
+     public Rectangle size = new Rectangle();
+    
+    public void skap(){
+
+    for (int i = 800; i < 0; i--)
+    {
+     size = new(random1.Next(0,500),800,80,5);
+    }
+    }
+
+    public void rita(){
+            Raylib.DrawRectangleRec(size, Color.Green);
+    }
     
 }
