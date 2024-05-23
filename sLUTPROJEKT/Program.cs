61b23678ebbd1329252f16ded6e8a76033284a72
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2024-05-23 11:59:19 +0200

    Fixa så att det är mer i program.cs igen ;(

--- sLUTPROJEKT/Program.cs
+++ sLUTPROJEKT/Program.cs
@@ -1,1 +1,34 @@
-﻿Game.WhileLoop();
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using Raylib_cs;
+
+// Använder Program.cs som källa för variablar
+
+int screenWidth = 500;
+int screenHeight = 900;
+
+Raylib.InitWindow(screenWidth, screenHeight, "Hello World");
+Raylib.SetTargetFPS(120);
+
+
+string scene = "start";
+Vector2 buttonSize = new(400, 100);
+Rectangle startButton = new Rectangle(screenWidth / 2 - buttonSize.X / 2, 600, buttonSize);
+
+int points = 0;
+int highScore = 0;
+
+
+Player player = new Player();
+Rectangle startGround = new Rectangle();
+
+// Har lista för att jag vill kunna ändra på den under kodens gång. Med aray skulle det inte gå :)
+List<Plattform> platforms = new();
+
+// 
+// 
+// Gör detta till sin egna metod ;)
+// 
+// 
+Game.WhileLoop(scene,player,points,startGround,highScore,platforms,startButton,screenHeight,screenWidth);
+
